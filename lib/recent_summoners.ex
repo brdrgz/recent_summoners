93c562d7febd39e3a7a4668bf4f601a5a2ae2f02
Blake Rodriguez <b.matt.rho@gmail.com>
2023-06-24 00:16:58 -0700

    Initial commit

---
+++ lib/recent_summoners.ex
@@ -0,0 +1,18 @@
+defmodule RecentSummoners do
+  @moduledoc """
+  Documentation for `RecentSummoners`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RecentSummoners.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/recent_summoners_test.exs
@@ -0,0 +1,8 @@
+defmodule RecentSummonersTest do
+  use ExUnit.Case
+  doctest RecentSummoners
+
+  test "greets the world" do
+    assert RecentSummoners.hello() == :world
+  end
+end
